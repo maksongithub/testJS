ff0caf9650f41b3c8149d9e15a6254ae310ec0d6
Maxim <makxim582@gmail.com>
2021-11-08 16:32:04 +0300

    First Task Complete

--- newScript.js
+++ newScript.js
@@ -1,3 +1,21 @@
 "use strict";
 
-console.log("Alex");
+let numberOfFilms = prompt("Сколько фильмов посмотрели?");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    private: false
+};
+
+let filmOne = prompt("Какой из последних фильмов вы посмотрели?");
+let filmOneStar = prompt("На скольков вы его оцените?");
+let filmTwo = prompt("Какой из последних фильмов вы посмотрели?");
+let filmTwoStar = prompt("На скольков вы его оцените?");
+
+personalMovieDB.movies[filmOne] = filmOneStar;
+personalMovieDB.movies[filmTwo] = filmTwoStar;
+
+console.log(personalMovieDB);
