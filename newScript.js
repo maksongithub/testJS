39cbb6ef3cfea5a7bbb7799cb21b9fb4fe5ebc88
Maxim <makxim582@gmail.com>
2021-11-08 16:45:08 +0300

    Task Two is complete

--- newScript.js
+++ newScript.js
@@ -10,12 +10,31 @@ let personalMovieDB = {
     private: false
 };
 
-let filmOne = prompt("Какой из последних фильмов вы посмотрели?");
-let filmOneStar = prompt("На скольков вы его оцените?");
-let filmTwo = prompt("Какой из последних фильмов вы посмотрели?");
-let filmTwoStar = prompt("На скольков вы его оцените?");
+for(let i = 0; i < 2; i++){
+    let film = prompt("Какой из последних фильмов вы посмотрели?");
+    let filmStar = prompt("На скольков вы его оцените?");
 
-personalMovieDB.movies[filmOne] = filmOneStar;
-personalMovieDB.movies[filmTwo] = filmTwoStar;
-
+    if(film == "" || filmStar == ""){
+        i--;
+        film = "", filmStar = "";
+        console.log("Error");
+    }
+    if(film.length>50){
+        i--;
+        film = "";
+        console.log("Error");
+    }
+    else{
+        personalMovieDB.movies[film] = filmStar;
+    }
+}
+if(personalMovieDB.count<=10){
+    alert("Вы посмотрели мало фильмов");
+}
+if(11<=personalMovieDB.count<=15){
+    alert("Вы видели не мало фильмов");
+}
+if(personalMovieDB.count>=16){
+    alert("Вы киноман");
+}
 console.log(personalMovieDB);
